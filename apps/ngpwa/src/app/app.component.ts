33272ce2edbfdda9767a504f5a062891122ba702
TmTron <martin.trummer@tmtron.com>
2020-12-17 10:30:02 +0100

    added init timer

--- apps/ngpwa/src/app/app.component.ts
+++ apps/ngpwa/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'pwa-update-test-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngpwa';
+export class AppComponent implements OnInit {
+  private timerId?: number;
+  status = 'uninit';
+
+  constructor(private readonly httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.status = 'waiting';
+    this.timerId = window.setTimeout(() => this.getAppInfo());
+  }
+
+  getAppInfo() {
+    this.status = 'working';
+    this.httpClient
+      .get('www.non-existing.com/end-point')
+      .toPromise()
+      .then((_) => (this.status = 'ok'))
+      .catch((err) => {
+        this.status =
+          new Date().toLocaleTimeString() + ' failed! (will retry soon)';
+        console.log('getAppInfo failed', err);
+        this.timerId = window.setTimeout(() => this.getAppInfo(), 2000);
+      });
+  }
 }
